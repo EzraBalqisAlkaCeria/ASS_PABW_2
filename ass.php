fbb569970fc6939485fb4be1f89e3538b2209396
Ezra Balqis Alka Ceria <160447519+EzraBalqisAlkaCeria@users.noreply.github.com>
2024-05-06 10:24:40 +0700

    Update ass.php

--- ass.php
+++ ass.php
@@ -5,3 +5,22 @@ $username = 'username';
 $password = 'password';
 $database = 'cendekia';
 $koneksi = new mysqli($host, $username, $password, $database);
+
+if ($koneksi->connect_error) {
+    die("Koneksi gagal: " . $koneksi->connect_error);
+}
+
+$query = "SELECT * FROM naskah";
+$result = $koneksi->query($query);
+
+if ($result->num_rows > 0) {
+    $manuscripts = array();
+    while($row = $result->fetch_assoc()) {
+        $manuscripts[] = $row;
+    echo json_encode($manuscripts);
+} else {
+    echo "0 results";
+}
+
+$koneksi->close();
+?>
