dfdc99bd8885541b3433d3a2178a32f04210ff51
Ezra Balqis Alka Ceria <160447519+EzraBalqisAlkaCeria@users.noreply.github.com>
2024-05-06 10:26:22 +0700

    Update asss.js

--- asss.js
+++ asss.js
@@ -1,1 +1,10 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var manuscripts = JSON.parse(this.responseText);
+        console.log(naskah);
+    }
+};
+xhr.open("GET", "ass.php", true);
+xhr.send();
 
